a59d45f628e4f0eedbde2aa5cfd96e65e3706d86
kisnows <yq12315@gmail.com>
2015-09-20 20:34:54 +0800

    'update'

---
+++ js/index.js
@@ -0,0 +1,26 @@
+/* global fullpage */
+"use strict";
+window.onload = function () {
+    fullpage.init('#sectionContent', {
+        beforeLeave: function (leaveIndex, nowIndex) {
+            if (nowIndex === 2) {
+                //console.log('You will leave page 2');
+            }
+            //console.log(this, leaveIndex, nowIndex);
+        },
+        afterLoad: function (afterIndex) {
+            if (afterIndex === 2) {
+                //console.log('You will go to page 2');
+            }
+            //console.log(this, afterIndex);
+        },
+        beforeSlideLeave: function (pageIndex, slideNow, slideAfter) {
+            var _this = this;
+            var SlideNow = document.querySelector('#nowSlide');
+            SlideNow.innerHTML = 'Slide' + slideAfter;
+        },
+        afterSlideLoad: function (pageIndex, slideIndex) {
+            var _this = this;
+        }
+    });
+};
