ab9d8c0cf2d5ee546915895c4c5641692ab77772
kisnows <yq12315@gmail.com>
2020-02-22 18:30:51 +0800

    fix travis

--- demo/demo01/js/index.js
+++ demo/demo01/js/index.js
@@ -1,27 +1,33 @@
 /* global fullpage */
 "use strict";
-window.onload = function () {
-  fullpage.init('#sectionContent', {
-    pageSpeed: 500,
-    beforeLeave: function (leaveIndex, nowIndex) {
-      if (nowIndex === 2) {
-        //console.log('You will leave page 2');
+window.onload = function() {
+  function fire() {
+    fullpage.init("#sectionContent", {
+      pageSpeed: 500,
+      beforeLeave: function(leaveIndex, nowIndex) {
+        if (nowIndex === 2) {
+          //console.log('You will leave page 2');
+        }
+        //console.log(this, leaveIndex, nowIndex);
+      },
+      afterLoad: function(afterIndex) {
+        if (afterIndex === 2) {
+          //console.log('You will go to page 2');
+        }
+        //console.log(this, afterIndex);
+      },
+      beforeSlideLeave: function(pageIndex, slideNow, slideAfter) {
+        var _this = this;
+        var SlideNow = document.querySelector("#nowSlide");
+        SlideNow.innerHTML = "Slide" + slideAfter;
+      },
+      afterSlideLoad: function(pageIndex, slideIndex) {
+        var _this = this;
       }
-      //console.log(this, leaveIndex, nowIndex);
-    },
-    afterLoad: function (afterIndex) {
-      if (afterIndex === 2) {
-        //console.log('You will go to page 2');
-      }
-      //console.log(this, afterIndex);
-    },
-    beforeSlideLeave: function (pageIndex, slideNow, slideAfter) {
-      var _this = this;
-      var SlideNow = document.querySelector('#nowSlide');
-      SlideNow.innerHTML = 'Slide' + slideAfter;
-    },
-    afterSlideLoad: function (pageIndex, slideIndex) {
-      var _this = this;
-    }
+    });
+  }
+  fire();
+  window.addEventListener("resize", () => {
+    fire();
   });
 };
